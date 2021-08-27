e05c45af79d61d54ab4ec27f3e4fa41b906c1161
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2021-08-27 12:11:06 +0200

    FIRST COMMIT

---
+++ src/CV/CV.js
@@ -0,0 +1,102 @@
+export const CV = {
+  hero: {
+    name: "Anthony",
+    adress: "Edward Stark",
+    city: "Avengers Tower / New York",
+    email: "tony@starkindustries.com",
+    birthDate: "29/05/1970",
+    phone: "(+12) 767333841",
+    image: "https://i.imgur.com/ZQAkED3.png",
+    gitHub: "https://github.com/tonystark",
+    aboutMe: [
+      {
+        info: "🤖 My armor, it was never a distraction or a hobby, it was a cocoon. I am Iron Man.",
+      },
+      {
+        info: "🔩 CEO of Stark Industries.",
+      },
+      {
+        info: "🕶 Genius, billionaire, playboy, philanthropist.",
+      },
+      {
+        info: "🦾 If we can’t protect the Earth, you can be damn well sure we’ll avenge it!",
+      },
+    ],
+  },
+  education: [
+    {
+      name: "Desarrollo Full Stack",
+      date: "2021",
+      where: "Upgrade Hub",
+    },
+    {
+      name: "Desarrollo de Aplicaciones Multiplataforma",
+      date: "2019-2021 (1er año concluido)",
+      where: "Cesur",
+    },
+    {
+      name: "Administración y Contabilidad",
+      date: "2013-2016",
+      where: "Maude",
+    },
+    {
+      name: "Técnico de Laboratorio de Imagen",
+      date: "2009-2011",
+      where: "IES Politécnico Jesús Marín",
+    },
+  ],
+  experience: [
+    {
+      name: "Auxiliar Administrativo",
+      date: "01/01/2019 – 28/02/2021",
+      where: "Escudero Administraciones",
+      description:
+        "Atención a proveedores y clientes personal/telefónica/e-mail, gestión de datos, informatización y digitalización de documentos, elaboración de convocatorias actas, archivado.",
+    },
+    {
+      name: "Auxiliar Administrativo",
+      date: "01/09/2013 – 01/01/2019",
+      where: "M&R Asociados",
+    },
+    {
+      name: "Aprendiz de pintor",
+      date: "01/09/2012 – 05/03/2013",
+      where: "Copimar",
+    },
+    {
+      name: "Reponedor nocturno",
+      date: "01/01/2012 – 01/09/2012",
+      where: "Supermercados Más",
+    },
+    {
+      name: "Ayudante de diseño gráfico (prácticas)",
+      date: "01/09/2011 – 01/05/2012",
+      where: "Peritec Corporation",
+    },
+    {
+      name: "Fotógrafo",
+      date: "01/09/2010 – 01/05/2011",
+      where: "Paco Herrera Fotografía",
+    },
+  ],
+  languages: {
+    language: "Inglés",
+    wrlevel: "Alto",
+    splevel: "Medio-Alto",
+  },
+  habilities: ["HTML5", "CSS3", "Git", "Angular", "React", "MongoDB", "Node", "PHP", "mySQL", "Ultimaker Cura", "Chitubox"],
+  volunteer: [
+    {
+      name: "Protectora de animales",
+      where: "Madrid",
+      description:
+        "Voluntario habitual colaborando con labores de mantenimiento, cuidado de animales y limpieza en protectoras de animales.",
+    },
+    {
+      name: "Impresión de material médico",
+      where: "Málaga",
+      description:
+        "Impresión en 3D de material médico tales como viseras, salvaorejas y piezas para su montaje a diversos centros de salud y hospitales a comienzos de 2020",
+    },
+  ],
+};
